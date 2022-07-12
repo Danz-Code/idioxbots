6b3f2356ca2f5641b155c9f592952b43ac7bbbd0
Danz-Code <55229337+Danz-Code@users.noreply.github.com>
2022-07-12 19:10:38 +0700

    Add files via upload

--- idioxbot.js
+++ idioxbot.js
@@ -1642,7 +1642,7 @@ const jumlahUser = pendaftar.length
   if (!isInventory){ addInventori(m.sender) }
   if (!isInventoriBuruan){ addInventoriBuruan(m.sender) }
   
-  const saldoku = Saldouser(userid)
+  const saldoku = Saldouser(m.sender)
   const notedong = `📮 *Don't spam command or you will got banned.*`
 const menulist = `
 *INFORMATION :* ${notebest}
